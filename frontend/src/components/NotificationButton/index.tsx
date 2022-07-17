8277357472192999ac6b35cbf63fba5b717f2b48
jrmazalli <jrmazalli@hotmail.com>
2022-07-17 20:11:36 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -12,11 +12,9 @@ type Props = {
 function handleClick(id : number){
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        toast.info("SMS enviado com sucesso");
+        toast.info("SMS enviado com sucesso ");
     });
 }
-
-
 
 function NotificationButton({saleId} : Props) {
     return (
